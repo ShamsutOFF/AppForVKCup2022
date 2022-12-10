1ce007fe48f193641b59334ae6e20c5a59218b1a
ShamsutOFF <shamsutoff@yandex.ru>
2022-12-10 09:19:00 +0300

    Настроил дизайн чекбоксов

--- app/src/main/java/com/example/myfirststageappforvkcup2022/MainActivity.kt
+++ app/src/main/java/com/example/myfirststageappforvkcup2022/MainActivity.kt
@@ -1,43 +1,136 @@
 package com.example.myfirststageappforvkcup2022
 
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myfirststageappforvkcup2022.ui.theme.MyFirstStageAppForVKCup2022Theme
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfirststageappforvkcup2022.ui.theme.*
+import com.google.accompanist.flowlayout.FlowRow
+
+private const val TAG = "@@@ MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyFirstStageAppForVKCup2022Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FlowRow(modifier = Modifier.padding(10.dp)) {
+                        createChips()
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun createChips() {
+    val categoriesList = getListOfCategories()
+    categoriesList.forEach { DrawCheckbox(it) }
 }
 
-@Preview(showBackground = true)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultPreview() {
-    MyFirstStageAppForVKCup2022Theme {
-        Greeting("Adel2")
+private fun DrawCheckbox(category: String) {
+    var selected by remember {
+        mutableStateOf(false)
     }
+
+    val sfproFont = FontFamily(Font(R.font.sfprodisp_regular))
+    val filterChipsColors = FilterChipDefaults.filterChipColors(
+        selectedContainerColor = Orange,
+        containerColor = TransparentWhite
+    )
+
+    FilterChip(
+        colors = filterChipsColors,
+        border = null,
+        selected = selected,
+        onClick = { selected = !selected },
+        label = {
+            Text(
+                fontSize = 16.sp,
+                color = White,
+                text = category,
+                fontFamily = sfproFont,
+                textAlign = TextAlign.Center
+            )
+        },
+        trailingIcon = if (selected) {
+            {
+                Icon(
+                    tint = White,
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Localized Description",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        } else {
+            {
+                Divider(
+                    color = White,
+                    modifier = Modifier
+                        .height(20.dp)
+                        .width(1.dp)
+                )
+                Icon(
+                    modifier = Modifier.size(18.dp),
+                    tint = White,
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Localized description"
+                )
+            }
+        }
+    )
+
+}
+
+fun getListOfCategories(): List<String> {
+    return listOf(
+        "Кино",
+        "Автомобили",
+        "Мотоциклы",
+        "Книги",
+        "Музыка",
+        "Кино",
+        "Сериалы",
+        "Спорт",
+        "Прогулки",
+        "Фотографии",
+        "Новости",
+        "Юмор",
+        "Политика",
+        "Рецепты",
+        "Еда",
+        "Отдых",
+        "Рестораны",
+        "Путешествия",
+        "История",
+        "Дети",
+        "Лайфхаки",
+        "Игры",
+        "Для взрослых 18+",
+        "Экономика",
+        "Технологии"
+    )
 }
