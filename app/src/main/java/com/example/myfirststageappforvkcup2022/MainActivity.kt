e165f89295efb8f1901a854deedbd1b19e53d913
ShamsutOFF <shamsutoff@yandex.ru>
2022-12-12 16:06:10 +0300

    Готово!)))

--- app/src/main/java/com/example/myfirststageappforvkcup2022/MainActivity.kt
+++ app/src/main/java/com/example/myfirststageappforvkcup2022/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -24,15 +27,14 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.myfirststageappforvkcup2022.ui.theme.*
 import com.google.accompanist.flowlayout.FlowRow
 
-private const val TAG = "@@@ MainActivity"
 
 private val categories = mutableListOf<String>()
 private val buttonEnabled = mutableStateOf(false)
 private val DIMENS = Dimens()
+val sfproFont = FontFamily(Font(R.font.sfprodisp_regular))
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,13 +58,59 @@ private fun MainScreen() {
                 .padding(dimensionResource(R.dimen.padding_medium))
         ) {
             item {
-                FlowRow (
-                    modifier = Modifier
-                        .padding(dimensionResource(R.dimen.padding_zero))
-                ){
+                FlowRow {
+                    CreateTopTextAndButton()
                     CreateChips()
                     DrawButton()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateTopTextAndButton() {
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier
+            .padding(
+                dimensionResource(id = R.dimen.padding_zero),
+                dimensionResource(id = R.dimen.padding_medium)
+            )
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(3f)
+        ) {
+            Text(
+                fontSize = DIMENS.FONT_SIZE_16,
+                color = TransparentWhiteTopText,
+                text = "Отметьте то, что вам интересно, чтобы настроить Дзен",
+                fontFamily = sfproFont,
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(1.5f)
+        )
+        {
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Click!", Toast.LENGTH_SHORT).show()
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = TransparentWhiteTopButton),
+            ) {
+                Text(
+                    fontSize = DIMENS.FONT_SIZE_16,
+                    color = Color.White,
+                    text = "Позже",
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
@@ -81,20 +129,21 @@ private fun DrawCheckbox(category: String) {
         mutableStateOf(false)
     }
 
-    val sfproFont = FontFamily(Font(R.font.sfprodisp_regular))
     val filterChipsColors = FilterChipDefaults.filterChipColors(
         selectedContainerColor = Orange,
-        containerColor = TransparentWhite
+        containerColor = TransparentWhiteChips
     )
 
     FilterChip(
         colors = filterChipsColors,
-        modifier = Modifier.padding(
-            dimensionResource(R.dimen.padding_zero),
-            dimensionResource(R.dimen.padding_small),
-            dimensionResource(R.dimen.padding_small),
-            dimensionResource(R.dimen.padding_zero)
-        ).height(dimensionResource(R.dimen.chip_height)),
+        modifier = Modifier
+            .padding(
+                dimensionResource(R.dimen.padding_zero),
+                dimensionResource(R.dimen.padding_small),
+                dimensionResource(R.dimen.padding_small),
+                dimensionResource(R.dimen.padding_zero)
+            )
+            .height(dimensionResource(R.dimen.chip_height)),
         border = null,
         selected = selected,
         onClick = {
@@ -109,7 +158,6 @@ private fun DrawCheckbox(category: String) {
                 color = White,
                 text = category,
                 fontFamily = sfproFont,
-                textAlign = TextAlign.Center
             )
         },
         trailingIcon = if (selected) {
--- app/src/main/java/com/example/myfirststageappforvkcup2022/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myfirststageappforvkcup2022/ui/theme/Theme.kt
@@ -8,13 +8,8 @@ import androidx.compose.runtime.Composable
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink80,
+    background = Black
 )
 
 @Composable
