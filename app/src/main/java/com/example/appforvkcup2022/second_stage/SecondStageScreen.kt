99eed23e9146e01e956f3eca30c55ab03cd16e1e
ShamsutOFF <shamsutoff@yandex.ru>
2023-01-07 21:03:32 +0300

    Звездочки почти готовы

--- app/src/main/java/com/example/appforvkcup2022/second_stage/SecondStageScreen.kt
+++ app/src/main/java/com/example/appforvkcup2022/second_stage/SecondStageScreen.kt
@@ -1,99 +1,163 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.example.appforvkcup2022.second_stage
 
-import android.content.res.Resources.Theme
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.RatingBar
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-import com.google.accompanist.pager.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun SecondStageScreen() {
-
-    val pagerState = rememberPagerState(0)
-    val scope = rememberCoroutineScope()
-    val scaffoldState = rememberScaffoldState()
-
     Scaffold(
-        scaffoldState = scaffoldState,
-        bottomBar = {
-            BottomAppBar(
-                backgroundColor = MaterialTheme.colorScheme.background
-            ) {
-                BottomTabRow(pagerState, scope)
-            }
-        },
-        modifier = Modifier.background(MaterialTheme.colorScheme.background),
-        content = {
-            HorizontalPager(5, state = pagerState) { page ->
-                when (page) {
-                    0 -> {
-                        CreatePage("Первая страница!")
-                    }
-                    1 -> {
-                        CreatePage("Вторая страница!")
-                    }
-                    2 -> {
-                        CreatePage("Третья страница!")
-                    }
-                    3 -> {
-                        CreatePage("Четвертая страница!")
-                    }
-                    4 -> {
-                        CreatePage("Пятая страница!")
-                    }
-                }
-            }
-        }
-    )
-}
-
-@ExperimentalPagerApi
-@Composable
-fun BottomTabRow(pagerState: PagerState, scope: CoroutineScope) {
-    TabRow(
-        selectedTabIndex = pagerState.currentPage,
-        indicator = { tabPositions ->
-            TabRowDefaults.Indicator(
-                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
-                height = 5.dp,
-                color = Color.Gray,
-            )
-
-        }
-
-    ) {
-        val pages = listOf("1й", "2й", "3й", "4й", "5й")
-        pages.forEachIndexed { index, title ->
-            Tab(
-                text = { androidx.compose.material.Text(title) },
-                selected = pagerState.currentPage == index,
-                onClick = { scope.launch { pagerState.scrollToPage(index) } },
-            )
-        }
-    }
-}
-
-@Composable
-fun CreatePage(text:String) {
-        Box(
-            modifier = Modifier
+        modifier = Modifier.fillMaxSize()
+    ) { padding ->
+        Column(
+            Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
-            contentAlignment = Alignment.Center
+                .background(MaterialTheme.colorScheme.background)
+                .padding(padding)
         ) {
-            Text(text = text, fontSize = 22.sp)
+            val pagerState = rememberPagerState()
+            val pages = listOf("1й", "2й", "3й", "4й", "5й")
+
+            HorizontalPager(
+                count = pages.size,
+                state = pagerState,
+                modifier = Modifier
+                    .weight(1f)
+//                    .background(Color.Yellow)
+                    .fillMaxWidth(),
+            ) { page ->
+                DrawStars()
+            }
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                activeColor = Color.White,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+//                    .background(Color.Green)
+                    .padding(16.dp, 16.dp, 16.dp, 64.dp),
+            )
         }
     }
+}
+
+@Composable
+fun DrawStars() {
+    LazyColumn() {
+        items(count = Int.MAX_VALUE) { count ->
+            ElevatedCard(modifier = Modifier.padding(3.dp)) {
+//                Text(text = "Some text in Card. Count: $count")
+                RatingBar(rating = 0)
+            }
+        }
+    }
+}
+
+@Composable
+fun RatingBar(
+    rating: Int
+) {
+    var animated by remember { mutableStateOf(false) }
+    val rotation = remember { Animatable(initialValue = 360f) }
+
+//    val transition = updateTransition(targetState = animated, label = "Star")
+//    val sizeValue by transition.animateDp(
+////    val sizeValue by transition.animateDp(
+////        transitionSpec = { repeatable(2,tween(durationMillis = 1000), repeatMode = RepeatMode.Reverse) },
+////        transitionSpec = { tween(durationMillis = 1000) },
+//
+//        label = "",
+//    ) { screenState ->
+//
+//        if (screenState) {
+//            72.dp
+//        } else {
+//            64.dp
+//        }
+//    }
+    
+    var ratingState by remember { mutableStateOf(rating) }
+    var selected by remember { mutableStateOf(false) }
+
+    LaunchedEffect(animated) {
+        rotation.animateTo(
+            targetValue = if (animated) 0f else 360f,
+            animationSpec = tween(durationMillis = 1000)
+        )
+    }
+
+    val size by animateDpAsState(
+        targetValue = if (selected) 72.dp else 64.dp,
+        spring(Spring.DampingRatioMediumBouncy)
+    )
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        for (i in 1..5) {
+            Icon(
+                painter = painterResource(
+                    id =
+                    if (i <= ratingState)
+                        com.example.appforvkcup2022.R.drawable.star_filled
+                    else com.example.appforvkcup2022.R.drawable.star_border
+                ),
+                contentDescription = "star",
+                modifier = Modifier
+//                    .size(sizeValue)
+                    .size(size)
+                    .graphicsLayer { rotationY = rotation.value }
+                    .pointerInteropFilter {
+                        println("it.action - ${it.action}")
+                        when (it.action) {
+                            MotionEvent.ACTION_DOWN -> {
+                                selected = true
+                                ratingState = i
+                                animated = !animated
+                            }
+                            MotionEvent.ACTION_UP -> selected = false
+//                            MotionEvent.ACTION_UP -> {
+//                                selected = true
+//                                ratingState = i
+//                                animated = !animated
+//                            }
+                            MotionEvent.ACTION_MOVE -> selected = false
+                            MotionEvent.ACTION_CANCEL -> {selected = false}
+                        }
+                        true
+                    },
+                tint = com.example.appforvkcup2022.ui.theme.Orange
+            )
+        }
+
+    }
+}
