5afdf2244b76532dedfa909f7669298bf90c8bfb
ShamsutOFF <shamsutoff@yandex.ru>
2023-01-10 20:16:42 +0300

    Сделал третий экран

--- app/src/main/java/com/example/appforvkcup2022/second_stage/pages/SecondPageComparisonBetwenTwoElements.kt
+++ app/src/main/java/com/example/appforvkcup2022/second_stage/pages/SecondPageComparisonBetwenTwoElements.kt
@@ -1,14 +1,17 @@
 package com.example.appforvkcup2022.second_stage.pages
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntOffset
@@ -21,7 +24,10 @@ private const val ROUND = 100
 
 @Composable
 fun DrawSecondPage() {
-    val context = LocalContext.current
+    var draggableComposable by remember {
+        mutableStateOf<(@Composable () -> Unit)?>(null)
+    }
+
     LazyColumn() {
         items(count = Int.MAX_VALUE) { count ->
             ElevatedCard(
@@ -36,17 +42,15 @@ fun DrawSecondPage() {
                         .height(300.dp)
                 ) {
                     val comparison = getRandomComparison()
-                    var x = 100.0f
-                    var y = 100.0f
-                    var id = 0
+                    val xCoordinates = listOf(100f, 900f)
+                    val yCoordinates = listOf(100f, 300f, 500f, 700f)
+                    var i = 0
                     comparison.pairs.forEach {
-                        DrawBubble(startValueX = x, startValueY = y, text = it.first, id = id)
-                        DrawBubble(startValueX = x+900, startValueY = y, text = it.second, id = id)
-                        y += 200
+                        DrawBubble(
+                            firstStartValue = Offset(xCoordinates[0], yCoordinates[i]),
+                            SecondStartValue = Offset(xCoordinates[1], yCoordinates[i++]), text = it
+                        )
                     }
-
-
-//Здесь было то что в низу закоментировано
                 }
             }
         }
@@ -54,29 +58,99 @@ fun DrawSecondPage() {
 }
 
 @Composable
-fun DrawBubble(startValueX: Float, startValueY: Float, text: String, id: Int) {
-    var offsetX by remember { mutableStateOf(startValueX) }
-    var offsetY by remember { mutableStateOf(startValueY) }
+fun DrawBubble(firstStartValue: Offset, SecondStartValue: Offset, text: Pair<String, String>) {
+    var firstOffset by remember { mutableStateOf(firstStartValue) }
+    var secondOffset by remember { mutableStateOf(SecondStartValue) }
+    var match by remember { mutableStateOf(false) }
+    val color by animateColorAsState(targetValue = if (match) Color.Green else White)
+
+    val randomPoint by remember {
+        mutableStateOf(getRandomOffsetOutsideTheScreen() )
+    }
+
+
+    val runawayOffsetFirst by animateOffsetAsState(
+        targetValue = if (match) randomPoint else Offset(500f,500f),
+//        targetValue = if (match) randomPoint else firstOffset,
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+    )
+    val runawayOffsetSecond by animateOffsetAsState(
+        targetValue = if (match) randomPoint else Offset(500f,500f),
+        animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
+    )
+
+
     OutlinedButton(onClick = {},
-        colors = ButtonDefaults.buttonColors(containerColor = White),
+        colors = ButtonDefaults.buttonColors(containerColor = color),
         modifier = Modifier
-            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+            .offset {
+                if (match)
+                    IntOffset(runawayOffsetFirst.x.roundToInt(), runawayOffsetFirst.y.roundToInt())
+                else
+                    IntOffset(firstOffset.x.roundToInt(), firstOffset.y.roundToInt())
+            }
             .pointerInput(Unit) {
                 detectDragGestures(
                     onDrag = { change, dragAmount ->
                         change.consume()
-                        offsetX += dragAmount.x
-                        offsetY += dragAmount.y
+                        firstOffset += dragAmount
                     },
                     onDragEnd = {
-                        //TODO вызвать коллбэк и отправить новые координаты с ID
+                        match = checkMatching(firstOffset, secondOffset)
                     }
                 )
             }
     ) {
-        Text(text = text)
+        Text(text = text.first)
+    }
+
+    OutlinedButton(onClick = {},
+        colors = ButtonDefaults.buttonColors(containerColor = color),
+        modifier = Modifier
+            .offset {
+                if (match)
+                    IntOffset(
+                        runawayOffsetSecond.x.roundToInt(),
+                        runawayOffsetSecond.y.roundToInt()
+                    )
+                else
+                    IntOffset(secondOffset.x.roundToInt(), secondOffset.y.roundToInt())
+            }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDrag = { change, dragAmount ->
+                        change.consume()
+                        secondOffset += dragAmount
+                    },
+                    onDragEnd = {
+                        match = checkMatching(firstOffset, secondOffset)
+                    }
+                )
+            }
+    ) {
+        Text(text = text.second)
     }
 }
+
+private fun getRandomOffsetOutsideTheScreen(): Offset {
+    println("getRandomOffsetOutsideTheScreen()")
+    val randomPoints = listOf(
+        Random.nextInt(1500, 6500).toFloat(),
+        Random.nextInt(-5500, -500).toFloat(),
+        Random.nextInt(1500, 6500).toFloat(),
+        Random.nextInt(-5500, -500).toFloat()
+    )
+    return Offset(
+        randomPoints[Random.nextInt(randomPoints.size)],
+        randomPoints[Random.nextInt(randomPoints.size)]
+    )
+}
+
+private fun checkMatching(firstOffset: Offset, secondOffset: Offset): Boolean {
+    return (firstOffset.x in secondOffset.x - ROUND..secondOffset.x + ROUND &&
+        firstOffset.y in secondOffset.y - ROUND..secondOffset.y + ROUND)
+}
+
 private fun getRandomComparison(): Comparison {
     val forms = getAllComparison()
     return forms[Random.nextInt(forms.size)]
@@ -98,75 +172,3 @@ private fun getAllComparison(): List<Comparison> {
 data class Comparison(
     val pairs: List<Pair<String, String>>,
 )
-
-
-
-//                    var offsetX by remember { mutableStateOf(0f) }
-//                    var offsetY by remember { mutableStateOf(0f) }
-//
-//                    var offsetX2 by remember { mutableStateOf(1000f) }
-//                    var offsetY2 by remember { mutableStateOf(0f) }
-//
-//                    OutlinedButton(onClick = {},
-//                        colors = ButtonDefaults.buttonColors(containerColor = White),
-//                        modifier = Modifier
-//                            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
-//                            .pointerInput(Unit) {
-//                                detectDragGestures(
-//                                    onDrag = { change, dragAmount ->
-//                                        change.consume()
-//                                        offsetX += dragAmount.x
-//                                        offsetY += dragAmount.y
-//                                    },
-//                                    onDragEnd = {
-//                                        if (offsetX in offsetX2 - ROUND..offsetX2 + ROUND &&
-//                                            offsetY in offsetY2 - ROUND..offsetY2 + ROUND
-//                                        ) {
-//                                            Toast
-//                                                .makeText(
-//                                                    context,
-//                                                    "СОВПАЛОСЬ FIRST",
-//                                                    Toast.LENGTH_SHORT
-//                                                )
-//                                                .show()
-//                                            println("detectDragGestures! onDragEnd!! FIRST")
-//                                            println("СОВПАЛОСЬ!!!")
-//                                        }
-//                                    }
-//                                )
-//                            }
-//                    ) {
-//                        Text(text = "FIRST")
-//                    }
-//
-//                    OutlinedButton(onClick = {},
-//                        colors = ButtonDefaults.buttonColors(containerColor = White),
-//                        modifier = Modifier
-//                            .offset { IntOffset(offsetX2.roundToInt(), offsetY2.roundToInt()) }
-//                            .pointerInput(Unit) {
-//                                detectDragGestures(
-//                                    onDrag = { change, dragAmount ->
-//                                        change.consume()
-//                                        offsetX2 += dragAmount.x
-//                                        offsetY2 += dragAmount.y
-//                                    },
-//                                    onDragEnd = {
-//                                        if (offsetX in offsetX2 - ROUND..offsetX2 + ROUND &&
-//                                            offsetY in offsetY2 - ROUND..offsetY2 + ROUND
-//                                        ) {
-//                                            Toast
-//                                                .makeText(
-//                                                    context,
-//                                                    "СОВПАЛОСЬ SECOND",
-//                                                    Toast.LENGTH_SHORT
-//                                                )
-//                                                .show()
-//                                            println("detectDragGestures! onDragEnd!! SECOND")
-//                                            println("СОВПАЛОСЬ!!! SECOND")
-//                                        }
-//                                    }
-//                                )
-//                            }
-//                    ) {
-//                        Text(text = "SECOND")
-//                    }
